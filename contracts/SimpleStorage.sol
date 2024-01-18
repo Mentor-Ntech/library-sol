d2d4a3fa17d5689a08e4c5242656c9390e7f2118
Mentor-Ntech <oloyedeaderayo11@gmail.com>
2024-01-18 14:44:22 +0100

    add commit

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.8;
 
 contract SimpleStorage {
 
-    uint256 public  favoriteNumber;
-    People public person = People({favoriteNumber: 2, name: "Dan", color:"red", fav_friend: "Umar", age: 30});
+    uint256  favoriteNumber;
+    //People public person = People({favoriteNumber: 2, name: "Dan", color:"red", fav_friend: "Umar", age: 30});
 
     struct People {
         uint256 favoriteNumber;
@@ -14,15 +14,23 @@ contract SimpleStorage {
         uint256 age;
     }
 
+    //ARRAY
+   // uint256[] public favoriteNumbersList;
+    People[] public people;
+
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
         //favoriteNumber = favoriteNumber +10;
         uint256 testVar = 5;
     }
 
+    //View, pure
     function retrieve() public view returns(uint256) {
         return favoriteNumber;
     }
 
-    
+    function addPerson(string memory _name, uint256 _favoriteNumber, string memory _color, string memory _fav_friend, uint256 _age) public {
+        People memory newPerson = People({favoriteNumber:_favoriteNumber, name:_name, color: _color, fav_friend: _fav_friend, age: _age});
+        people.push(newPerson);
+    }
 }
