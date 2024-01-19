c06d07fb20c317bc0f8ecb41839947d2e702a387
Mentor-Ntech <oloyedeaderayo11@gmail.com>
2024-01-19 12:32:09 +0100

    simple-commit

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -21,7 +21,7 @@ contract SimpleStorage {
    // uint256[] public favoriteNumbersList;
     People[] public people;
 
-    function store(uint256 _favoriteNumber) public {
+    function store(uint256 _favoriteNumber) public  virtual {
         favoriteNumber = _favoriteNumber;
         //favoriteNumber = favoriteNumber +10;
         // uint256 testVar = 5;
