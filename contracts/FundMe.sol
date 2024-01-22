a4122cbb3bf3448a2e2a4be8900b5c7dfcd5c76b
Mentor-Ntech <oloyedeaderayo11@gmail.com>
2024-01-22 15:38:39 +0100

    init-commit

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,10 +5,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
 
+    using  PriceConverter for uint256;
     uint256 public minimumUsd = 50 * 1e18;
     
     address[] public funders;
@@ -18,34 +19,13 @@ contract FundMe {
     function fund() public payable {
         // Want to be able to set a minimum amount
         // 1. How do we send ETH to this contract
-        try this.getConversionRate(msg.value) returns (uint256 conversionRate) {
-            require(conversionRate >= minimumUsd, "Didn't send enough");
+        require (msg.value.getConversionRate() >= minimumUsd, "Didn't send enough");
             funders.push(msg.sender);
             addressToAmountFunded[msg.sender] = msg.value;
-        } catch Error(string memory errorMessage) {
-            emit ErrorOccurred(errorMessage);
-            // You can handle the error or take other actions here
-        } catch (bytes memory) {
-            // Executed for other errors not caught by the specific catch clauses
-        }
+        
     }
 
-    function getPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int price,, ,) = priceFeed.latestRoundData();
-        return uint256(price * 1e10);
-    }
-
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
-    }
+    
 
     // function withdraw() {}
 }
